b9f8e7db033efc60bf50492058c777117ff1c18b
Somnath Das <37792778+SomnathDas@users.noreply.github.com>
2023-05-10 14:22:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,24 @@ const App = () => {
     firstCard: {
       head: "AI + RPA is what we do",
       body: "Future-Proof your business. Drive efficiency, profitability and deliver on customer experience",
-      image: { img1 },
       linkObj: { link: "", title: "AI + RPA Automation" },
     },
 
     secondCard: {
       head: "Make Bolder Choices",
       body: "Digital focused strategies to realize market-changing ideas",
-      image: { img2 },
       linkObj: { link: "", title: "Build Better Apps" },
     },
 
     thirdCard: {
       head: "Innovate with Speed",
       body: "Create higher quality software, deliver on customer expectations and business goals",
-      image: { img3 },
       linkObj: { link: "", title: "DevOps" },
     },
 
     fourthCard: {
       head: "Embrace Cloud",
       body: "With Cloud-First accelerate innovation and optimize performance",
-      image: { img4 },
       linkObj: { link: "", title: "Cloud Services" },
     },
   };
@@ -42,28 +38,28 @@ const App = () => {
       <Card
         head={CardObject.firstCard.head}
         body={CardObject.firstCard.body}
-        image={CardObject.firstCard.image}
+        image={img1}
         linkObj={CardObject.firstCard.linkObj}
       />
 
       <Card
         head={CardObject.secondCard.head}
         body={CardObject.secondCard.body}
-        image={CardObject.secondCard.image}
+        image={img2}
         linkObj={CardObject.secondCard.linkObj}
       />
 
       <Card
         head={CardObject.thirdCard.head}
         body={CardObject.thirdCard.body}
-        image={CardObject.thirdCard.image}
+        image={img3}
         linkObj={CardObject.thirdCard.linkObj}
       />
 
       <Card
         head={CardObject.fourthCard.head}
         body={CardObject.fourthCard.body}
-        image={CardObject.fourthCard.image}
+        image={img4}
         linkObj={CardObject.fourthCard.linkObj}
       />
     </section>
